1db9f2ec3c8deddcfd5b51fe8140e153e0f7493f
Verlif <verlif@outlook.com>
2025-08-19 10:27:33 +0800

    修改上下文以适配自动部署

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,16 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+
+// 根据环境变量设置 base（GitHub Pages 部署需要）
+const base = process.env.NODE_ENV === 'production' ? '/one/' : '/'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
   ],
+  base, // 关键配置：生产环境路径前缀
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
