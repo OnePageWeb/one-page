81afdc7af7d5c9bfde6886cd63624a6c59756aea
Verlif <verlif@outlook.com>
2025-09-24 19:37:46 +0800

    修复资源上下文问题

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,27 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig, loadEnv } from 'vite'
+import {fileURLToPath, URL} from 'node:url'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-    // 在回调函数中可以安全访问 mode
-    let base = '/'
+export default defineConfig(({mode}) => {
+  console.log('当前模式:', mode) // 确认 mode
+  // 在回调函数中可以安全访问 mode
+  let base = mode === 'production' ? '/one/' : '/'
 
-    return {
-        plugins: [vue()],
-        base, // 关键配置：生产环境路径前缀
-        resolve: {
-            alias: {
-                '@': fileURLToPath(new URL('./src', import.meta.url)),
-            },
-        },
-    }
+  if (mode === 'dev') {
+    // 映射为标准的 development 环境
+    process.env.NODE_ENV = 'development'
+    base = '/'
+  }
+  console.log('当前base:', base) // 确认 mode
+
+  return {
+    plugins: [vue()],
+    base, // 关键配置：生产环境路径前缀
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  }
 })
